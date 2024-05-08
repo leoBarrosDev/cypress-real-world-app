43445c332435bbb49c7ce6db1555a5191bed4e88
Leandro Reis <leohbr@gmail.com>
2024-05-08 09:12:51 -0300

    add enviroments

--- cypress/tests/ui/user.spec.ts
+++ cypress/tests/ui/user.spec.ts
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const env = require('../../../cypress.env.json');
+
 describe("User tests", function () {
     beforeEach(() => {
         cy.task("db:seed");
@@ -15,8 +17,8 @@ describe("User tests", function () {
         cy.get('#firstName').type('Leandro');
         cy.get('#lastName').type('Reis');
         cy.get('#username').type('leandro.reis');
-        cy.get('#password').type('s3cret', { log: false });
-        cy.get('#confirmPassword').type('s3cret');
+        cy.get('#password').type(env.password_default, { log: false });
+        cy.get('#confirmPassword').type(env.password_default, { log: false });
         cy.get('[data-test="signup-submit"]').click();
         cy.location('pathname')
             .should('eq', '/signin');
