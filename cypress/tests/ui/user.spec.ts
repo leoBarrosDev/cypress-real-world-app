fe9c3e157b4ea52378f6ae53471e505c9f619bb2
Leandro Reis <leohbr@gmail.com>
2024-05-08 14:34:54 -0300

    add User register with blank field test

--- cypress/tests/ui/user.spec.ts
+++ cypress/tests/ui/user.spec.ts
@@ -6,14 +6,14 @@ describe("User tests", function () {
     beforeEach(() => {
         cy.task("db:seed");
         cy.visit('/');
-    })
-
-    it('User register successfuly', () => {
         cy.title()
             .should('eq', 'Cypress Real World App');
         cy.get("[data-test='signup']").click();
         cy.location('pathname')
             .should('eq', '/signup');
+    })
+
+    it('User register successfuly', () => {
         cy.get('#firstName').type('Leandro');
         cy.get('#lastName').type('Reis');
         cy.get('#username').type('leandro.reis');
@@ -22,5 +22,37 @@ describe("User tests", function () {
         cy.get('[data-test="signup-submit"]').click();
         cy.location('pathname')
             .should('eq', '/signin');
+    })
+
+    it.only('User register with blank field', () => {
+        const requiredFields = [
+            { field: '#firstName', name: 'First Name' },
+            { field: '#lastName', name: 'Last Name' },
+            { field: '#username', name: 'Username' },
+            { field: '#password', name: 'Password' },
+        ];
+
+        requiredFields.forEach((requiredField, index) => {
+            cy.get(requiredField.field).click();
+            if (index < requiredFields.length - 1) {
+                const nextField = requiredFields[index + 1];
+                cy.get(nextField.field).click();
+                cy.get(`${requiredField.field}-helper-text.Mui-error`)
+                    .should('be.visible')
+                    .should('contain', `${requiredField.name} is required`);
+            }
+        })
+    })
+
+    it('User register with inválid password', () => {
+
+    })
+
+    it("User register with passwords that don't match", () => {
+
+    })
+
+    it('user register with the “confirm password” field blank', () => {
+
     })
 })
