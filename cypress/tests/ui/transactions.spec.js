3c25e844d68e1554c67e6c88a766a683b3ee34ee
Leandro Reis <leohbr@gmail.com>
2024-05-18 16:16:47 -0300

    update transactions tests

--- cypress/tests/ui/transactions.spec.js
+++ cypress/tests/ui/transactions.spec.js
@@ -1,11 +1,10 @@
 /// <reference types="cypress" />
 
 const env = require('../../../cypress.env.json');
-import { faker } from '@faker-js/faker'
 
 describe('Transactions', () => {
 
-    it("Transaction's history made", () => {
+    it.only("Transaction's history made", () => {
         cy.visit('/');
         cy.title()
             .should('eq', 'Cypress Real World App');
@@ -19,9 +18,17 @@ describe('Transactions', () => {
             .should('be.visible').should('contain', 'Personal');
         cy.get("[data-test='transaction-list']")
             .should('be.visible')
+
+        cy.get('[data-test="transaction-amount-J5Fd3dlBEBu"]')
+            .should('be.visible')
+            .should('have.css', 'color', 'rgb(76, 175, 80)');
+
+        cy.get('[data-test="transaction-amount-2Lz6Q3zj4Rb"]')
+            .should('be.visible')
+            .should('have.css', 'color', 'rgb(255, 0, 0)');
     })
 
-    it.only("Transaction's history blank", () => {
+    it("Transaction's history blank", () => {
         const firstName = 'Leandro';
         const lastName = 'Reis';
         const username = 'leandro.reis';
@@ -40,21 +47,6 @@ describe('Transactions', () => {
         cy.get('#password').type(env.default_password, { log: false }).type('{enter}');
         cy.location('pathname')
             .should('eq', '/');
-
-        // cy.get('[data-test="user-onboarding-dialog-title"] > .MuiTypography-root')
-        //     .should('be.visible')
-        //     .should('contain', `Get Started with Real World App`);
-        // cy.get('[data-test="user-onboarding-next"] > .MuiButton-label').click();
-
-        // cy.get('[data-test="user-onboarding-dialog-title"] > .MuiTypography-root')
-        //     .should('be.visible')
-        //     .should('contain', `Create Bank Account`);
-
-        // cy.get('#bankaccount-bankName-input').type('Meu Banco');
-        // cy.get('#bankaccount-routingNumber-input').type(faker.finance.routingNumber());
-        // cy.get('#bankaccount-accountNumber-input').type(faker.finance.account(9));
-        // cy.get('[data-test="bankaccount-submit"] > .MuiButton-label').click();
-        // cy.get('[data-test="user-onboarding-next"] > .MuiButton-label').click();
 
         cy.get('[data-test="nav-personal-tab"] > .MuiTab-wrapper').click();
         cy.get('[data-test="empty-list-header"] > .MuiTypography-root')
